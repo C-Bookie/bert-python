ea1b0b41e2a223b865173b6c16f2feeebbed04cd
Calloumi-Bambooki <duck-vader@hotmail.co.uk>
2018-01-13 14:39:40 +0000

    adding text to display

--- pi_io.py
+++ pi_io.py
@@ -20,9 +20,13 @@ def init():
     global image
     image = Image.new('1', (width, height))
 
-    global pixels
-    pixels = image.load()
-#    draw = ImageDraw.Draw(image)
+    global font
+    font = ImageFont.load_default()
+
+#    global pixels
+#    pixels = image.load()
+    global draw
+    draw = ImageDraw.Draw(image)
 #    draw.rectangle((0,0,width,height), outline=0, fill=0)
 
 
@@ -35,27 +39,23 @@ def draw(red, green, blue, C, T):
     disp.clear()
 
     y = 0
-    while True:
+    while (y * subWidth) + x < len(C):
         x = 0
         while x < subWidth:
             p = C[(y * subWidth) + x] > T/2 if 1 else 0
-#            p = (x+y)%2
-#            p = x%2
-#            p = y%2
-            box(x, y, size, p)
-#            pixels[x, y] = p
-#            draw.point([x, y], 1)
+#            box(x, y, size, p)
+            draw.rectangle((x*size, y*size, size, size), outline=0, fill=p)
             x += 1
-            if (y * subWidth) + x >= len(C):
-                return
         y += 1
+    draw.text((x, y), "Red: " + str(red) + " Green: " + str(green) + " Blue: " + str(blue), font=font, fill=255)
+
 
 def box(x, y, s, p):
     py=0
     while py < s:
         px=0
         while px < s:
-            pixels[(x*s)+px, (y*s)+py] = p
+#            pixels[(x*s)+px, (y*s)+py] = p
             px+=1
         py+=1
 
